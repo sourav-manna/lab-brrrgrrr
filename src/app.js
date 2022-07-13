66ac893608e661056f73040713d9dad20d07802d
sourav-manna <sourav.manna2000@outlook.com>
2022-07-13 17:02:26 +0530

    sourav manna | first commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 
 //Initial price of the burger
 var wholeWheatBun = 10;
-
+var total_amount = 170;
 //Ingredients of the burger along with the price
 // Clue: the name is same as the textcontent of the button. Will be useful later on :)
 var ingredients = {
@@ -35,53 +35,216 @@ function renderAll() {
   renderPrice();
 }
 
+/*use for render ingredients*/
+
+
+/*function for paddy*/
 function renderPatty() {
-  let patty = document.querySelector("#patty");
-  //you can also use getElementById
-  if (state.Patty) {
-    patty.style.display = "inherit";
-  } else {
-    patty.style.display = "none";
+  const top = document.createElement('img')
+  top.src = "/img/Burger/Top@2x.png" 
+  const burger = document.getElementById("ingredients");
+
+  console.log("checked")
+  let item = document.getElementById("patty");
+  if(item == null){
+  /*remove burger top bread*/
+  burger.removeChild(burger.firstElementChild);
+  /*add selected item*/
+  item = document.createElement('img')
+  item.src = "/img/Burger/Patty@2x.png";
+  item.id = "patty"
+  burger.prepend(item);
+  /*add burger top*/
+  burger.prepend(top);
+  /*add price of item*/
+  renderPrice("add", ingredients.Patty)
+  }else{
+    item.remove();
+    /*substract price of item*/
+    renderPrice("sub", ingredients.Patty)
   }
+  renderIngredientsBoard("Patty")
 }
 
 function renderCheese() {
-  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  const top = document.createElement('img')
+  top.src = "/img/Burger/Top@2x.png" 
+  const burger = document.getElementById("ingredients");
+
+  let item = document.getElementById("cheese");
+  if(item == null){
+  /*remove burger top bread*/
+  burger.removeChild(burger.firstElementChild);
+  /*add selected item*/
+  item = document.createElement('img')
+  item.src = "/img/Burger/Cheese@2x.png";
+  item.id = "cheese"
+  burger.prepend(item);
+  /*add burger top*/
+  burger.prepend(top);
+  /*add price of item*/
+  renderPrice("add", ingredients.Cheese)
+  }else{
+    item.remove();
+    /*substract price of item*/
+    renderPrice("sub", ingredients.Cheese)
+  }
+  renderIngredientsBoard("Cheese")
 }
 
 function renderTomatoes() {
-  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  const top = document.createElement('img')
+  top.src = "/img/Burger/Top@2x.png" 
+  const burger = document.getElementById("ingredients");
+
+  let item = document.getElementById("tomato");
+  if(item == null){
+  /*remove burger top bread*/
+  burger.removeChild(burger.firstElementChild);
+  /*add selected item*/
+  item = document.createElement('img')
+  item.src = "/img/Burger/Tomato@2x.png";
+  item.id = "tomato"
+  burger.prepend(item);
+  /*add burger top*/
+  burger.prepend(top);
+  /*add price of item*/
+  renderPrice("add", ingredients.Tomatoes)
+  }else{
+    item.remove();
+    /*substract price of item*/
+    renderPrice("sub", ingredients.Tomatoes)
+  }
+  renderIngredientsBoard("Tomatoes")
 }
 
 function renderOnions() {
-  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  const top = document.createElement('img')
+  top.src = "/img/Burger/Top@2x.png" 
+  const burger = document.getElementById("ingredients");
+
+  let item = document.getElementById("onion");
+  if(item == null){
+  /*remove burger top bread*/
+  burger.removeChild(burger.firstElementChild);
+  /*add selected item*/
+  item = document.createElement('img')
+  item.src = "/img/Burger/Onion@2x.png";
+  item.id = "onion"
+  burger.prepend(item);
+  /*add burger top*/
+  burger.prepend(top);
+  /*add price of item*/
+  renderPrice("add", ingredients.Onions)
+  }else{
+    item.remove();
+    /*substract price of item*/
+    renderPrice("sub", ingredients.Onions)
+  }
+  renderIngredientsBoard("Onions");
 }
 
 function renderLettuce() {
-  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  const top = document.createElement('img')
+  top.src = "/img/Burger/Top@2x.png" 
+  const burger = document.getElementById("ingredients");
+
+  let item = document.getElementById("lettuce");
+  if(item == null){
+  /*remove burger top bread*/
+  burger.removeChild(burger.firstElementChild);
+  /*add selected item*/
+  item = document.createElement('img')
+  item.src = "/img/Burger/Lettuce@2x.png";
+  item.id = "lettuce"
+  burger.prepend(item);
+  /*add burger top*/
+  burger.prepend(top);
+  /*add price of item*/
+  renderPrice("add", ingredients.Lettuce)
+  }else{
+    item.remove();
+    /*substract price of item*/
+    renderPrice("sub", ingredients.Lettuce)
+  }
+  renderIngredientsBoard("Lettuce")
 }
 
 document.querySelector(".btn-patty").onclick = function () {
-  state.Patty = !state.Patty;
-  renderAll();
+  renderPatty();
+  renderButtons(".btn-patty")
 };
 
 // Trial 2 - Setup event listener for the cheese button
-
+document.querySelector(".btn-cheese").onclick = function () {
+  renderCheese();
+  renderButtons(".btn-cheese")
+};
 
 // Trial 2 - Setup event listener for the tomatoes button
-
+document.querySelector(".btn-tomatoes").onclick = function () {
+  renderTomatoes();
+  renderButtons(".btn-tomatoes")
+};
 
 // Trial 2 - Setup event listener for the onion button
-
+document.querySelector(".btn-onions").onclick = function () {
+  renderOnions();
+  renderButtons(".btn-onions")
+};
 
 // Trial 2 - Setup event listener for the lettuce button
-
+document.querySelector(".btn-lettuce").onclick = function () {
+  renderLettuce();
+  renderButtons(".btn-lettuce");
+};
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
-
+function renderButtons(button){
+  const btn = document.querySelector(button)
+  if(btn.classList.contains("active")){
+    btn.classList.remove('active')
+  }else{
+    btn.classList.add('active')
+  }
+}
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+function renderPrice(mode, amount){
+  let price = total_amount;
+  if(mode == "add"){
+    price += amount
+  }else if(mode == "sub"){
+    price -= amount
+  }
+  total_amount = price
+  document.getElementById("payable").innerHTML="INR "+total_amount
+}
+
+function renderIngredientsBoard(item){
+  const header = document.getElementById("heading")
+  const list_ing =  document.getElementsByClassName("items")
+  const newitem = document.createElement('p')
+  newitem.className = "items"
+  newitem.innerHTML = item
+  console.log(list_ing)
+  let flag = 0;
+  for(let i =0;i<list_ing.length;i++){
+      if(item == list_ing[i].innerHTML){
+          list_ing[i].remove();
+          flag = 1
+      }
+  }
+
+  if(flag == 0){
+    header.remove()
+    document.getElementById("menu").prepend(newitem)
+    document.getElementById("menu").prepend(header)
+  }
+
+}
+
 
 
 //Judgement 1
